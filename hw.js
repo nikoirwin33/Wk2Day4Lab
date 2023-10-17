50778936f040e64b9687419ca6cea27a2db28e69
nikoirwin33 <146851989+nikoirwin33@users.noreply.github.com>
2023-10-16 22:57:56 -0400

    Homework10/16

--- hw.js
+++ hw.js
@@ -17,23 +17,60 @@
 //     }else if(x <= 55){
 //          console.log("Bad Grade but you'll get em next time!")
 //     }
-//     }
-    
+//     
 //     Grade(80)
 
 // 2.   Write a function that prints out multiples of 10 up to a given input (argument)
 
+  // function countByTen(){
+  //   for(let i = 0; i <= 100; i = i + 10){
+  //     console.log(i)
+  //   if(i ==100){
+  //         i = 100 
+  //     }
+  // }
+  // }
+  
+  // countByTen()
+   
+  
 
 
 // 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
-let i = 0
-while (i < 8- bagels){
-    quarters += 5
-    i++
-}
-console.log(quarters)
+
+      // function change(x, y){
+       //   let z = 0
+      //   let i = 0
+     //   while (i < y - x){
+    //       z += 4
+    //       i++
+    //   }
+       // console.log(z)
+      // }
+
+      // change(20,50)
+
+//4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paid and amount cost
+
+//  
 
 
-//4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paind and amount cost
+
+
+
+
 
 // 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+for(let i = 0; i <=100; i++){
+    
+  if(i % 3 == 0 && i % 5 == 0){
+      console.log(i + ' FizzBuzz')
+  }else if(i % 5== 0 && i % 3 !== 0){
+      console.log(i + ' Buzz')
+  }else if(i % 3 == 0){
+      console.log(i + ' Fizz')
+  }else{
+      console.log(i)
+}
+} 
